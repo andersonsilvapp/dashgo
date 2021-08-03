df56972a8ed3b7aa731f2b02d68ceb09de0b08b7
Anderson Apparecido <andersonsilvapp@gmail.com>
2021-08-03 00:31:44 -0300

    responseive sidebar drawer added

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { createContext, ReactNode } from "react";
+
+interface SidebarDrawerContextProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData)
+
+export function SidebarDrawerProvider({ children }: SidebarDrawerContextProps) {
+  const disclosure = useDisclosure()
+  const router = useRouter()
+
+  useEffect(() => {
+    disclosure.onClose()
+  }, [router.asPath])
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  )
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext)
